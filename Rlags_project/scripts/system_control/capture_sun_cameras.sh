bbc7e48036575b8c4d599757b01eb1a2dd264962
linaro <rrfullmer@usu.edu>
2014-05-29 20:09:12 -0600

    waiting notification on capture_sun_cameras

--- Rlags_project/scripts/system_control/capture_sun_cameras.sh
+++ Rlags_project/scripts/system_control/capture_sun_cameras.sh
@@ -4,6 +4,7 @@ start=$(date +%s)
 sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_0.sh > cameras.txt 2> /dev/null
 sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_1.sh > cameras.txt 2> /dev/null
 sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_2.sh > cameras.txt 2> /dev/null
+echo "waiting..."
 	while [ $(expr $(date +%s) - $start)  -lt 10 ]; do
 		sleep 0.001
 	done
