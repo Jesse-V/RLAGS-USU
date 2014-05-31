387c13303018ec93f56485b1e679596a43974b29
linaro <rrfullmer@usu.edu>
2014-05-31 17:14:37 -0600

    scripts able to query two out of three sun cameras

--- Rlags_project/scripts/sun_cameras/capture_sun_1.sh
+++ Rlags_project/scripts/sun_cameras/capture_sun_1.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-sudo echo '1-2.1.5.1' | sudo tee /sys/bus/usb/drivers/usb/unbind
-sudo echo '1-2.1.5.2' | sudo tee /sys/bus/usb/drivers/usb/bind
-sudo echo '1-2.1.5.3' | sudo tee /sys/bus/usb/drivers/usb/unbind
-sudo echo '1-2.1.5.4' | sudo tee /sys/bus/usb/drivers/usb/unbind
+sudo echo '1-2.1.3.1' | sudo tee /sys/bus/usb/drivers/usb/unbind
+sleep 0.2
+sudo echo '1-2.1.3.2' | sudo tee /sys/bus/usb/drivers/usb/unbind
+sleep 0.2
+sudo echo '1-2.1.3.3' | sudo tee /sys/bus/usb/drivers/usb/unbind
+sleep 0.2
+sudo echo '1-2.1.3.2' | sudo tee /sys/bus/usb/drivers/usb/bind
+
 sleep 0.5
+
 sudo /home/linaro/Rlags_project/scripts/sun_cameras/get_sun1_image
 #sudo /home/linaro/Rlags_project/scripts/sun_cameras/get_angle.sh
 sudo /home/linaro/Rlags_project/scripts/sun_cameras/rename_image1.sh
--- Rlags_project/scripts/system_control/capture_sun_cameras.sh
+++ Rlags_project/scripts/system_control/capture_sun_cameras.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-while true
-do
+#while true
+#do
 	start=$(date +%s)
 
-	sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_0.sh > cameras.txt 2> /dev/null
-	sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_1.sh > cameras.txt 2> /dev/null
-	sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_2.sh > cameras.txt 2> /dev/null
+	#sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_0.sh > cameras.txt
+	#sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_1.sh > cameras.txt
+	#sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_2.sh > cameras.txt
+
+	sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_0.sh
+        sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_1.sh
+        sudo /home/linaro/Rlags_project/scripts/sun_cameras/capture_sun_2.sh
 
 	echo "waiting..."
 	while [ $(expr $(date +%s) - $start)  -lt 10 ]; do
 		sleep 0.001
 	done
-done
+#done
