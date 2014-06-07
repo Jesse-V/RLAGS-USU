40a7484b577ddbf269789e84140380ef1f4d0023
linaro <rrfullmer@usu.edu>
2014-06-06 22:15:06 -0600

    cleaned, fixed, and recompiled sun camera code

--- Rlags_project/Sun_Camera/rlags_code/sun_camera2.cpp
+++ Rlags_project/Sun_Camera/rlags_code/sun_camera2.cpp
@@ -6,22 +6,22 @@
 
 int  main()
 {
-	int width = 1280;
-	int height = 960;
-	int CamNum=0;
-	openCamera(CamNum);
+	const int WIDTH = 1280, HEIGHT = 960;
+
+	openCamera(0);
 	initCamera(); //this must be called before camera operation. and it only need init once
-	setImageFormat(width, height, 1, IMG_RAW8);
-	IplImage *pRgb;
-	pRgb=cvCreateImage(cvSize(width,height), IPL_DEPTH_8U, 1);
+	setImageFormat(WIDTH, HEIGHT, 1, IMG_RAW8);
+	IplImage *pRgb = cvCreateImage(cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 1);
 	setValue(CONTROL_EXPOSURE, 1000, true); //auto exposure
+
 	startCapture(); //get image
 	getImageData((unsigned char*)pRgb->imageData, pRgb->imageSize, -1);
 	getImageData((unsigned char*)pRgb->imageData, pRgb->imageSize, -1);
 	getImageData((unsigned char*)pRgb->imageData, pRgb->imageSize, -1);
 	getImageData((unsigned char*)pRgb->imageData, pRgb->imageSize, -1);
-	stopCapture();
 	cvSaveImage("/home/linaro/Rlags_project/scripts/sun_cameras/sun_cam_2.jpg",pRgb);
+	stopCapture();
+
 	return 1;
 }
 
